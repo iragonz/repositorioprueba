49e237bae0d7b772d75f4cc95f322502ad1b7c82
ivan <ivanramosgonz@gmail.com>
2021-10-08 15:18:27 +0000

    58

---
+++ holamundoidioma.php
@@ -0,0 +1,11 @@
+<?php
+    $idioma = $_GET["idioma"];
+    echo $idioma;
+    $es = "hola";
+    $en = "hello";
+    $fr = "Hallo";
+    $fr = "bojour";
+    $it = "ciao";
+    $mostrar = $idioma;
+    echo $$mostrar;
+?>
